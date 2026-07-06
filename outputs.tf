4a6b7a150b1c26db582c2ae65b8df28ce8140440
Daniel Allen <dan@aeternagroup.net>
2026-07-06 23:11:13 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -2,4 +2,20 @@ output "application_security_groups" {
   description = "All application_security_group resources"
   value       = azurerm_application_security_group.application_security_groups
 }
+output "application_security_groups_location" {
+  description = "List of location values across all application_security_groups"
+  value       = [for k, v in azurerm_application_security_group.application_security_groups : v.location]
+}
+output "application_security_groups_name" {
+  description = "List of name values across all application_security_groups"
+  value       = [for k, v in azurerm_application_security_group.application_security_groups : v.name]
+}
+output "application_security_groups_resource_group_name" {
+  description = "List of resource_group_name values across all application_security_groups"
+  value       = [for k, v in azurerm_application_security_group.application_security_groups : v.resource_group_name]
+}
+output "application_security_groups_tags" {
+  description = "List of tags values across all application_security_groups"
+  value       = [for k, v in azurerm_application_security_group.application_security_groups : v.tags]
+}
 
