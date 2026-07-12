3aecc72e78a33d38809d6a1b108d6bc65284b3e5
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:15:56 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,21 +1,17 @@
-output "application_security_groups" {
-  description = "All application_security_group resources"
-  value       = azurerm_application_security_group.application_security_groups
-}
 output "application_security_groups_location" {
-  description = "List of location values across all application_security_groups"
-  value       = [for k, v in azurerm_application_security_group.application_security_groups : v.location]
+  description = "Map of location values across all application_security_groups, keyed the same as var.application_security_groups"
+  value       = { for k, v in azurerm_application_security_group.application_security_groups : k => v.location }
 }
 output "application_security_groups_name" {
-  description = "List of name values across all application_security_groups"
-  value       = [for k, v in azurerm_application_security_group.application_security_groups : v.name]
+  description = "Map of name values across all application_security_groups, keyed the same as var.application_security_groups"
+  value       = { for k, v in azurerm_application_security_group.application_security_groups : k => v.name }
 }
 output "application_security_groups_resource_group_name" {
-  description = "List of resource_group_name values across all application_security_groups"
-  value       = [for k, v in azurerm_application_security_group.application_security_groups : v.resource_group_name]
+  description = "Map of resource_group_name values across all application_security_groups, keyed the same as var.application_security_groups"
+  value       = { for k, v in azurerm_application_security_group.application_security_groups : k => v.resource_group_name }
 }
 output "application_security_groups_tags" {
-  description = "List of tags values across all application_security_groups"
-  value       = [for k, v in azurerm_application_security_group.application_security_groups : v.tags]
+  description = "Map of tags values across all application_security_groups, keyed the same as var.application_security_groups"
+  value       = { for k, v in azurerm_application_security_group.application_security_groups : k => v.tags }
 }
 
